47aa72d7736a8e6c68d08581e8523919328ddce4
jlbrnh <jl.br.nh@gmail.com>
2014-09-19 07:44:10 -0300

    -

--- src/TextChocolate.js
+++ src/TextChocolate.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var fs = require('fs');
+var crypto = require('crypto');
 
 function TextChocolate(translationJsonFile, i18n) {
     var stats = fs.lstat(translationJsonFile);
@@ -11,11 +12,32 @@ function TextChocolate(translationJsonFile, i18n) {
     this.defaultLang = 'en-US';
     this.lang(i18n);
 
-    (function transformTheJsonFileIntoHashFunctions () {
-        var json = JSON.parse(fs.readFileSync(translationJsonFile, 'utf8'));
-    }())
+    (function transformJsonFile2HashFunctions () {
+        var i, json = JSON.parse(fs.readFileSync(translationJsonFile, 'utf8'));
+        for (i in json) {
+            console.log(i);
+        }
+    }());
 }
 
 TextChocolate.prototype.lang = function (i18n) {
     this.i18n = i18n || this.defaultLang;
 };
+
+TextChocolate.prototype.hash = function (msg) {
+    return crypto.createHash('sha1').update(msg).digest('hex');
+};
+
+TextChocolate.prototype.translate = function (msg, n) {
+    if (!msg) {
+        return '';
+    }
+    try {
+        
+    } catch (e) {
+        // any error will return the original msg 
+        return msg;
+    }
+};
+
+module.exports = TextChocolate;
